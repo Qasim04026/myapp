0ed6de1a1cbd6915e737fd72f0697f56dbee4d22
Qasim04026 <161979120+Qasim04026@users.noreply.github.com>
2024-10-15 02:13:46 +0500

    Add files via upload
    add files of next.js

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Nestedroute from "./(mainpages)/about/nestedroute/page";
+import Link from "./components/Navbar";
+
+export default function() {
+  return(
+    <div>
+      i am homepage <br />
+    
+      <Navbar />
+    </div>
+  )
+}
