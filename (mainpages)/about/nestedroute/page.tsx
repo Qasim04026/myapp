e8688fe98b3e946277a3bc99d2dc91f3e44ec1c5
Qasim04026 <161979120+Qasim04026@users.noreply.github.com>
2024-10-15 02:20:51 +0500

    Add files via upload

---
+++ (mainpages)/about/nestedroute/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Nestedroute = () => {
+    return(
+        <div>
+    <h1 className="text-white"> This is Nested Route!!!</h1>
+        </div>
+    )
+}
+export default Nestedroute;
---
+++ (mainpages)/about/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import pic1 from "../../public/pic1.webp"
+const About = ()=>{
+    return(
+        <div>
+            <h1>About</h1>
+            
+
+        </div>
+    )
+}
+export default About;
